8aef1bae2f8b25739521cb24979ee13a5deed678
ErikRossi331 <erik_rossisilva@outlook.com>
2022-01-27 01:21:10 -0300

    Alterações no Script

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,43 @@
-alert('alerta')
+var nome = "Erik Rossi"
+var idade = 16;
+var idade2 = 10;
+var numero1 = "20 ";
+var numero2 = "30";
+var frase= "Meu nome é Caio Rossi."
+//alert( nome + " tem "  + idade  + " anos e seu "irmão" tem" + idade2);
+//alert(idade + idade2);
+
+//alert (numero1 + numero2);
+console.log(nome);
+console.log(idade);
+console.log(idade2);
+//console.log(frase);
+//console.log(frase.toUpperCase);
+//console.log(frase.toLowerCase);
+console.log(frase.replace("Caio", "Erik"));
+
+
+var lista = ["maçã", "pêra", "melancia"];
+lista.push("uva")
+//lista.pop("uva");
+console.log(lista);
+console.log(lista.toString());
+//console.log(lista[1]);
+//console.log(lista[2]);
+//console.log(lista[3]);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" "))
+
+
+var fruta = [{nome: "maçã", cor: "vermelha"}, {nome: "banana", cor: "amarela"}]
+console.log(fruta);
+console.log(fruta[0]);
+console.log(fruta[1]);
+//console.log(fruta[1].nome);
+//console.log(fruta[1].cor);
+//console.log(fruta[0].nome);
+//console.log(fruta[0].cor);
+
+
